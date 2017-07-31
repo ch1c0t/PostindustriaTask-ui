70657f8c2ca6cd63031bc8c6b3da54f6bcb27115
Anatoly Chernow <chertoly@gmail.com>
2017-08-01 02:31:31 +0300

    Sketch up a table

--- ui.rb
+++ ui.rb
@@ -1,9 +1,23 @@
 require 'hobby'
+require 'slim'
+
+class Page
+  def initialize path
+    @template = Slim::Template.new path
+  end
+
+  def to_html
+    @template.render self
+  end
+end
+
+HTML = Page.new('index.slim').to_html
 
 class UI
   include Hobby
 
   get do
-    "The user interface will be here."
+    content_type :html
+    HTML
   end
 end
