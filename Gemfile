364833be4c29f9c59cea8549208551b0c7da51c6
Anatoly Chernow <chertoly@gmail.com>
2017-08-01 23:35:42 +0300

    Styling with Zurb Foundation

--- Gemfile
+++ Gemfile
@@ -1,5 +1,12 @@
-source 'https://rubygems.org'
+source 'https://rubygems.org' do
+  gem 'hobby'
+  gem 'puma'
+  gem 'slim'
+  gem 'sass'
+  gem 'foundation-scss'
+end
 
-gem 'hobby'
-gem 'puma'
-gem 'slim'
+source 'https://rails-assets.org' do
+  gem 'rails-assets-jquery'
+  gem 'rails-assets-teacup'
+end
--- ui.rb
+++ ui.rb
@@ -1,9 +1,20 @@
 require 'hobby'
 require 'slim'
 
+require 'foundation/scss/in/sass_path'
+
 class Page
   def initialize path
     @template = Slim::Template.new path
+  end
+
+  def css_tag
+    sass_string = IO.read 'css/main.sass'
+    css_string = Sass::Engine.new(sass_string, load_paths: ['css']).render
+    "<style>#{css_string}</style>"
+  end
+
+  def js_tag
   end
 
   def to_html
